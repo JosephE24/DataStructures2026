6ef7a4ad1b6d7ff3025b2483106accb664894db7
JosephE24 <jellis34@atu.edu>
2026-01-15 12:07:34 -0600

    Update test_spring_26_practice.cpp

--- test_spring_26_practice/test_spring_26_practice.cpp
+++ test_spring_26_practice/test_spring_26_practice.cpp
@@ -7,6 +7,7 @@ int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "Test Message!\n";
+    std::cout << "Test Message 2!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
